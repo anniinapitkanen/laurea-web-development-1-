4854a1592b7a50dbe6f7ab6401146ef899ff7737
Anniina <anniina.pitkanen@student.laurea.fi>
2026-09-16 15:52:37 +0300

    anniina

--- WS02_JS_basics/js/script.js
+++ WS02_JS_basics/js/script.js
@@ -1,2 +1,9 @@
-console.log("Hello World!");
-alert("Hello World!");
+const name = "Anniina";
+let age = 25;
+const favouriteAnimal = "dog";
+
+console.log(name);
+console.log(age);
+console.log(favouriteAnimal);
+
+console.log("Hello! My name is " + name + " and my favourite animal is the " + favouriteAnimal + ".");
