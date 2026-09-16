b7698262a28bd0db5daa244e4075d05e46b315c4
Anniina <anniina.pitkanen@student.laurea.fi>
2026-09-16 15:54:52 +0300

    3

--- WS02_JS_basics/js/script.js
+++ WS02_JS_basics/js/script.js
@@ -1,3 +1,9 @@
+// Exercise 1 – Developer Tools and Console
+console.log("Hello World!");
+alert("Hello World!");
+
+
+// Exercise 2 – Variables
 const name = "Anniina";
 let age = 25;
 const favouriteAnimal = "dog";
@@ -7,3 +13,8 @@ console.log(age);
 console.log(favouriteAnimal);
 
 console.log("Hello! My name is " + name + " and my favourite animal is the " + favouriteAnimal + ".");
+
+
+// Exercise 3 – User Input
+const userName = prompt("What is your name?");
+console.log("Hello " + userName + "! Welcome to JavaScript.");
