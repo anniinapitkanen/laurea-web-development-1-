3932a779234915ba2ed5119a7b5d0906a4aa0822
Anniina <anniina.pitkanen@student.laurea.fi>
2026-09-23 18:40:46 +0300

    a

---
+++ WS03_JS_events/js/script.js
@@ -0,0 +1,22 @@
+function showTable() {
+    const animal = "Tiikeri";
+    const habitat = "Metsä";
+    const diet = "Liha";
+
+    const table = `
+        <table>
+            <tr>
+                <th>Eläin</th>
+                <th>Elinympäristö</th>
+                <th>Ruokavalio</th>
+            </tr>
+            <tr>
+                <td>${animal}</td>
+                <td>${habitat}</td>
+                <td>${diet}</td>
+            </tr>
+        </table>
+    `;
+
+    document.querySelector("#tableContainer").innerHTML = table;
+}
